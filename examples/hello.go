ae9a1a07ef1bc6398095b239c1be06b8673eee72
Jan Bader <jan.bader@ub-software.de>
2020-08-02 12:08:14 +0200

    Add int-argument example

--- examples/hello.go
+++ examples/hello.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/JaCoB1123/web"
 )
 
-func hello(val string) string { return "hello " + val + "\n" }
+func hello(val string) string {
+	return "hello " + val + "\n"
+}
+
+func helloInt(intval int, val string) string {
+	return fmt.Sprintf("hello %s (%d)\n", val, intval)
+}
 
 func main() {
+	web.Get("/([-+]?[0-9]*)/(.*)", helloInt)
 	web.Get("/(.*)", hello)
 	web.Run("0.0.0.0:9999")
 }
