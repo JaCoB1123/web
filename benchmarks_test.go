9640d6ab043842c58f595720f4d1fcc88dc120da
Jan Bader <jan.bader@ub-software.de>
2020-08-06 14:27:19 +0200

    Add tests for functions with context

--- benchmarks_test.go
+++ benchmarks_test.go
@@ -8,14 +8,14 @@ import (
 	"testing"
 )
 
-func BenchmarkHelloWorldTextonly(b *testing.B) {
+func BenchmarkWithContextHelloWorldTextonly(b *testing.B) {
 	s := NewServer()
 	s.SetLogger(log.New(ioutil.Discard, "", 0))
-	s.Get("/([-+]?[0-9]*)/(.*)", func(intval int, val string) string {
-		return fmt.Sprintf("hello %s (%d)\n", val, intval)
+	s.Get("/([-+]?[0-9]*)/(.*)", func(ctx *Context, intval int, val string) {
+		fmt.Fprintf(ctx.ResponseWriter, "hello %s (%d)\n", val, intval)
 	})
-	s.Get("/(.*)", func(val string) string {
-		return "hello " + val + "\n"
+	s.Get("/(.*)", func(ctx *Context, val string) {
+		fmt.Fprint(ctx.ResponseWriter, "hello "+val+"\n")
 	})
 	s.initServer()
 
@@ -29,7 +29,84 @@ func BenchmarkHelloWorldTextonly(b *testing.B) {
 		s.Process(&c, req)
 	}
 }
-func BenchmarkHelloWorldNumber(b *testing.B) {
+func BenchmarkWithContextHelloWorldNumber(b *testing.B) {
+	s := NewServer()
+	s.SetLogger(log.New(ioutil.Discard, "", 0))
+	s.Get("/([-+]?[0-9]*)/(.*)", func(ctx *Context, intval int, val string) {
+		fmt.Fprintf(ctx.ResponseWriter, "hello %s (%d)\n", val, intval)
+	})
+	s.Get("/(.*)", func(ctx *Context, val string) {
+		fmt.Fprint(ctx.ResponseWriter, "hello "+val+"\n")
+	})
+	s.initServer()
+
+	req := buildTestRequest("GET", "/123456/world", "", nil, nil)
+	var buf bytes.Buffer
+	iob := ioBuffer{input: nil, output: &buf}
+	c := dummyConnection{wroteHeaders: false, req: req, headers: make(map[string][]string), fd: &iob}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.Process(&c, req)
+	}
+}
+
+func BenchmarkWithContextProcessGet(b *testing.B) {
+	s := NewServer()
+	s.SetLogger(log.New(ioutil.Discard, "", 0))
+	s.Get("/echo/(.*)", func(ctx *Context, val string) {
+		fmt.Fprint(ctx.ResponseWriter, val)
+	})
+	req := buildTestRequest("GET", "/echo/hi", "", nil, nil)
+	var buf bytes.Buffer
+	iob := ioBuffer{input: nil, output: &buf}
+	c := dummyConnection{wroteHeaders: false, req: req, headers: make(map[string][]string), fd: &iob}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.Process(&c, req)
+	}
+}
+
+func BenchmarkWithContextProcessPost(b *testing.B) {
+	s := NewServer()
+	s.SetLogger(log.New(ioutil.Discard, "", 0))
+	s.Post("/echo/(.*)", func(ctx *Context, val string) {
+		fmt.Fprint(ctx.ResponseWriter, val)
+	})
+	req := buildTestRequest("POST", "/echo/hi", "", nil, nil)
+	var buf bytes.Buffer
+	iob := ioBuffer{input: nil, output: &buf}
+	c := dummyConnection{wroteHeaders: false, req: req, headers: make(map[string][]string), fd: &iob}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.Process(&c, req)
+	}
+}
+
+func BenchmarkWithReturnHelloWorldTextonly(b *testing.B) {
+	s := NewServer()
+	s.SetLogger(log.New(ioutil.Discard, "", 0))
+	s.Get("/([-+]?[0-9]*)/(.*)", func(ctx *Context, intval int, val string) {
+		fmt.Fprintf(ctx.ResponseWriter, "hello %s (%d)\n", val, intval)
+	})
+	s.Get("/(.*)", func(ctx *Context, val string) {
+		fmt.Fprint(ctx.ResponseWriter, "hello "+val+"\n")
+	})
+	s.initServer()
+
+	req := buildTestRequest("GET", "/world", "", nil, nil)
+	var buf bytes.Buffer
+	iob := ioBuffer{input: nil, output: &buf}
+	c := dummyConnection{wroteHeaders: false, req: req, headers: make(map[string][]string), fd: &iob}
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.Process(&c, req)
+	}
+}
+func BenchmarkWithReturnHelloWorldNumber(b *testing.B) {
 	s := NewServer()
 	s.SetLogger(log.New(ioutil.Discard, "", 0))
 	s.Get("/([-+]?[0-9]*)/(.*)", func(intval int, val string) string {
@@ -51,7 +128,7 @@ func BenchmarkHelloWorldNumber(b *testing.B) {
 	}
 }
 
-func BenchmarkProcessGet(b *testing.B) {
+func BenchmarkWithReturnProcessGet(b *testing.B) {
 	s := NewServer()
 	s.SetLogger(log.New(ioutil.Discard, "", 0))
 	s.Get("/echo/(.*)", func(s string) string {
@@ -68,7 +145,7 @@ func BenchmarkProcessGet(b *testing.B) {
 	}
 }
 
-func BenchmarkProcessPost(b *testing.B) {
+func BenchmarkWithReturnProcessPost(b *testing.B) {
 	s := NewServer()
 	s.SetLogger(log.New(ioutil.Discard, "", 0))
 	s.Post("/echo/(.*)", func(s string) string {
